5179d27570b74b90de6f6e801b6ecd2feb37e588
klmyoungyun <kyoungyun98@naver.com>
2024-06-27 01:18:51 +0900

    🎨 디자인시스템 액션시트 및 프리뷰 구현

--- Projects/DesignSystem/DesignSystemUI/Sources/ContentView.swift
+++ Projects/DesignSystem/DesignSystemUI/Sources/ContentView.swift
@@ -42,6 +42,7 @@ struct ContentView: View {
         NavigationLink("TabBar") { TabBarPreview() }
         NavigationLink("Toast") { ToastPreview() }
         NavigationLink("ClipboardToast") { ClipboardToastPreview() }
+        NavigationLink("ActionSheet") { ActionSheetPreview() }
     }
 }
 
