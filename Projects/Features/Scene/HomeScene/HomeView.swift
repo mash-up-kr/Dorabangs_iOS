6775ec9174a9d57e64207c2aaebed9ece23d02fe
Sanghee <95578975+AhnSangHee@users.noreply.github.com>
2024-06-29 14:10:02 +0900

    ♻️indices 사용하도록 수정
    Co-authored-by: 김영균 <48887389+klmyoungyun@users.noreply.github.com>

--- Projects/Features/Scene/HomeScene/HomeView.swift
+++ Projects/Features/Scene/HomeScene/HomeView.swift
@@ -30,7 +30,7 @@ public struct HomeView: View {
                 LazyVStack(pinnedViews: .sectionHeaders) {
                     Section {
                         LazyVStack(spacing: 0) {
-                            ForEach(0..<store.cards.count, id: \.self) { index in
+                            ForEach(store.cards.indices, id: \.self) { index in
                                 LKCard(
                                     title: "에스파 '슈퍼노바', 올해 멜론 주간 차트 최장 1위…'쇠맛' 흥행 질주에스파 '슈퍼노바', 올해 멜론 주간 차트 최장 1위…'쇠맛' 흥행 질주 에스파 '슈퍼노바', 올해 멜론 주간 차트 최장 1위…'쇠맛' 흥행 질주",
                                     description: "사건은 다가와 아 오 에 거세게 커져가 아 오 에 That tick, that tick, tick bomb That tick, that tick, tick bomb 사건은 다가와 아 오 에 거세게 커져가 아 오 에 That tick, that tick, tick bomb That tick, that tick, tick bomb",
