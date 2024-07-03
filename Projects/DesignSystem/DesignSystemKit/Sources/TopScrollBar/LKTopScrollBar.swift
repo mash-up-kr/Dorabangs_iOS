8575d1c3acb2ec53795a5ec3e38ba4c4720f476d
Sanghee <95578975+AhnSangHee@users.noreply.github.com>
2024-07-03 13:11:17 +0900

    ✨ LKTopScrollBar 구현 (#35)
    * 🎨아이콘 추가
    
    * 🎨LKTopTabView 구현
    
    * ✨LKTopScrollBar 구현
    
    * ✨LKTopScrollBar 구현
    
    * ✨AddTextOptionView 추가

---
+++ Projects/DesignSystem/DesignSystemKit/Sources/TopScrollBar/LKTopScrollBar.swift
@@ -0,0 +1,50 @@
+//
+//  LKTopScrollBar.swift
+//  DesignSystemUI
+//
+//  Created by 안상희 on 6/30/24.
+//  Copyright © 2024 mashup.dorabangs. All rights reserved.
+//
+
+import SwiftUI
+
+public struct LKTopScrollBar: View {
+    private let titleList: [String]
+    @State var selectedIndex: Int
+
+    public init(
+        titleList: [String],
+        selectedIndex: Int
+    ) {
+        self.titleList = titleList
+        self.selectedIndex = selectedIndex
+    }
+
+    public var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(titleList.indices, id: \.self) { index in
+                    LKTopTabView(
+                        isSelected: selectedIndex == index,
+                        title: titleList[index],
+                        count: ""
+                    )
+                    .frame(height: 50)
+                    .onTapGesture {
+                        self.selectedIndex = index
+                    }
+                }
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 8)
+            .background(DesignSystemKitAsset.Colors.white.swiftUIColor)
+        }
+    }
+}
+
+#Preview {
+    LKTopScrollBar(
+        titleList: ["전체", "즐겨찾기", "나중에 읽을 링크", "나즁에 또 읽을 링크", "영원히 안 볼 링크"],
+        selectedIndex: 0
+    )
+}
--- Projects/DesignSystem/DesignSystemUI/Sources/Common/Option.swift
+++ Projects/DesignSystem/DesignSystemUI/Sources/Common/Option.swift
@@ -11,9 +11,11 @@ import SwiftUI
 enum Option: View {
     case toggle(description: String, isOn: Binding<Bool>)
     case picker(description: String, cases: [Any], selectedIndex: Binding<Int>)
+    case addText(description: String, textList: Binding<[String]>, text: Binding<String>)
     case textField(description: String, text: Binding<String>)
     case optionalString(description: String, text: Binding<String?>)
     case optionalInt(description: String, value: Binding<Int?>)
+    case int(description: String, value: Binding<Int>)
 
     @ViewBuilder
     var body: some View {
@@ -22,12 +24,16 @@ enum Option: View {
             ToggleOptionView(description: description, isOn: isOn)
         case let .picker(description, cases, selectedIndex):
             PickerOptionView(description: description, cases: cases, selectedIndex: selectedIndex)
+        case let .addText(description: description, textList: textList, text: text):
+            AddTextOptionView(description: description, textList: textList, text: text)
         case let .textField(description, text):
             TextFieldOptionView(description: description, text: text)
         case let .optionalString(description, text):
             OptionalStringOptionView(description: description, text: text)
         case let .optionalInt(description, value):
             OptionalIntOptionView(description: description, value: value)
+        case let .int(description: description, value: value):
+            IntOptionView(description: description, value: value)
         }
     }
 }
