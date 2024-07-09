17ebbb78c75df2dd2e2a4ab8de589132288b2b9f
김영균 <48887389+klmyoungyun@users.noreply.github.com>
2024-07-09 20:35:19 +0900

    🎨 온보딩 디자인 변경 (#62)
    * 🎨 온보딩 디자인 변경
    
    * ✨빌드 오류 수정

--- Projects/DesignSystem/DesignSystemKit/Sources/ViewModifiers/Border.swift
+++ Projects/DesignSystem/DesignSystemKit/Sources/ViewModifiers/Border.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 public extension View {
     func border(_ content: some ShapeStyle, width: CGFloat = 1, cornerRadius: CGFloat) -> some View {
-        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
-        return clipShape(roundedRect)
-            .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+        let roundedCorner = RoundedCorner(radius: cornerRadius, corners: .allCorners)
+        return clipShape(roundedCorner)
+            .overlay(roundedCorner.stroke(content, lineWidth: width))
     }
 }
