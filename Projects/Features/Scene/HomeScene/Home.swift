af07f0933c4276cbf989e3aa6958c5a9deab52f1
Sanghee <dev.sanghee@gmail.com>
2024-07-15 01:41:33 +0900

    ♻️ 홈 탭 초기값 수정

--- Projects/Features/Scene/HomeScene/Home.swift
+++ Projects/Features/Scene/HomeScene/Home.swift
@@ -90,8 +90,8 @@ public struct Home {
             switch action {
             case .onAppear:
                 let home = [
-                    Folder(id: "", name: "모든 링크", type: .default, postCount: 0),
-                    Folder(id: "", name: "즐겨찾기", type: .default, postCount: 0),
+                    Folder(id: "", name: "모든 링크", type: .all, postCount: 0),
+                    Folder(id: "", name: "즐겨찾기", type: .favorite, postCount: 0),
                     Folder(id: "", name: "나중에 읽을 링크", type: .default, postCount: 0)
                 ]
                 state.tabs = HomeTab.State(tabs: home)
