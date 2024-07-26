536036465436a5d07c210a0fdbdcabc557a48ce4
Sanghee <dev.sanghee@gmail.com>
2024-07-27 03:16:19 +0900

    ✨ 홈 - 안 읽은 갯수 API 연결

--- Projects/Features/Scene/FeedScene/FeedView.swift
+++ Projects/Features/Scene/FeedScene/FeedView.swift
@@ -48,12 +48,12 @@ public struct FeedView: View {
 
                                 ForEach(store.cards.indices, id: \.self) { index in
                                     LKCard(
-                                        aiStatus: .success,
+                                        aiStatus: .inProgress,
                                         progress: 1.0,
                                         title: "에스파 '슈퍼노바', 올해 멜론 주간 차트 최장 1위…'쇠맛' 흥행 질주에스파 '슈퍼노바', 올해 멜론 주간 차트 최장 1위…'쇠맛' 흥행 질주 에스파 '슈퍼노바', 올해 멜론 주간 차트 최장 1위…'쇠맛' 흥행 질주",
                                         description: "사건은 다가와 아 오 에 거세게 커져가 아 오 에 That tick, that tick, tick bomb That tick, that tick, tick bomb 사건은 다가와 아 오 에 거세게 커져가 아 오 에 That tick, that tick, tick bomb That tick, that tick, tick bomb",
                                         thumbnailImage: { ThumbnailImage(urlString: "") },
-                                        tags: ["# 에스파", "# SM", "# 오에이옹에이옹"],
+                                        tags: ["에스파", "SM", "오에이옹에이옹"],
                                         category: "Category",
                                         timeSince: "1일 전",
                                         bookMarkAction: { store.send(.bookMarkButtonTapped(index)) },
